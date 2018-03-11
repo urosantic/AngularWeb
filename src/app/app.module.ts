9252ce3949f0a6c642c1c52fc79dfa8a555c51cc
urosantic <urosantic@hotmail.com>
2018-03-10 23:30:29 -0500

    started work on website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import {SideMenuComponent} from "./sideMenu/sideMenu.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sideMenu/sideMenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'side-menu',
+  templateUrl:'./sideMenu.component.html',
+  styleUrls:['./sideMenu.component.css']
+})
+
+export class SideMenuComponent {
+
+}
