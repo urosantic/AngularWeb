4f1d2aca6e35e29d98e37c3fbe8fb23f6608bb17
Uros Antic <urosantic@Daniels-Mac-mini.local>
2018-03-13 15:42:37 -0400

    more work done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {SideMenuComponent} from "./sideMenu/sideMenu.component";
 import {TopPageComponent} from "./topPage/topPage.component";
+import {ServicesComponent} from "./services/services.component";
+import {ArsenalComponent} from "./arsenal/arsenal.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SideMenuComponent,
-    TopPageComponent
+    TopPageComponent,
+    ServicesComponent,
+    ArsenalComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/arsenal/arsenal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'arsenal',
+  templateUrl:'./arsenal.component.html',
+  styleUrls:['./arsenal.component.css']
+})
+
+export class ArsenalComponent {
+
+}
